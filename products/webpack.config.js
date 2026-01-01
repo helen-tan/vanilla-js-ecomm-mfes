3f82e3c4dba43dafdfb6d1254bb3e63d39457af7
Helen Tan <helentanmz0909@gmail.com>
2026-01-01 22:28:57 +0800

    build: [PRODUCTS] - add Module Federation to expose products module - ProductsIndex, as a remote module

--- products/webpack.config.js
+++ products/webpack.config.js
@@ -1,4 +1,5 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
 
 module.exports = {
     mode: 'development',
@@ -8,6 +9,14 @@ module.exports = {
     plugins: [
         new HtmlWebpackPlugin({
             template: './public/index.html'
+        }),
+        // [REMOTE]: Module Federation Plugin to expose which modules/files you want to make available to other projects at runtime
+        new ModuleFederationPlugin({
+            name: 'products',
+            filename: 'remoteEntry.js',
+            exposes: {
+                './ProductsIndex': './src/index'
+            }
         })
     ]
 };
