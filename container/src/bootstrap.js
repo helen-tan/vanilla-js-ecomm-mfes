d4dcb5d77c094d11075e390215e2889ecb8c284b
Helen Tan <helentanmz0909@gmail.com>
2026-01-11 17:17:48 +0800

    feat: [PRODUCTS] - reliably render products when developing in isolation & when in container

--- container/src/bootstrap.js
+++ container/src/bootstrap.js
@@ -1,4 +1,8 @@
-import 'products/ProductsIndex';
+import { mount } from 'products/ProductsIndex';
 import 'cart/CartShow';
 
 console.log('container');
+
+// Specify element in index.html where the mfe can be safely rendered
+const productsMFE = document.querySelector('#products-mfe');
+mount(productsMFE);
--- products/src/bootstrap.js
+++ products/src/bootstrap.js
@@ -1,10 +1,42 @@
 import faker from 'faker';
 
-let products = '';
 
-for (let i=0; i < 5; i++) {
-    const name = faker.commerce.productName();
-    products += `<div>${name}</div>`
+const mount = (elem) => {
+    let products = '';
+    
+    for (let i=0; i < 5; i++) {
+        const name = faker.commerce.productName();
+        products += `<div>${name}</div>`
+    }
+    
+    elem.innerHTML = products;
+};
+
+// Problem with writing the line:
+// document.querySelector("#dev-products").innerHTML = products;
+
+// Situation #1
+// --------------
+// We are running this file in development in isolation
+// We are using our local index.html file
+// Which DEFINITELY has an element with an id of 'dev-products'
+// We want to immediately render our app into that element
+
+// Check if products is running in isolation
+if (process.env.NODE_ENV === 'development') { // env var that is set automatically by webpack, as we have added a mode : 'development' in our webpack file
+    const elem = document.querySelector('#dev-products');
+
+    // If running in isolation (running only products project)
+    if (elem) {        // assuming container doesnt have an element of id='dev-products'
+        mount(elem);
+    }
 }
 
-document.querySelector("#dev-products").innerHTML = products;
+
+// Situation #2
+// -------------
+// We are running this file in development or production
+// through the CONTAINER aoo
+// NO GUARANTEE that an element with an id of 'dev-products' exist
+// We DO NOT WANT to try to immediately render the app
+export { mount };
